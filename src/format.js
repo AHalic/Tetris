74055997da05e7f3cb4e2c56d3346787f2832711
AHalic <sophie.gama@aluno.ufes.br>
2021-09-06 02:28:16 -0300

    Modifica cores de peças

--- src/format.js
+++ src/format.js
@@ -1,8 +1,12 @@
 import Block from './block.js';
 
+//  _   <- Roxo  
+// ---  
 export class Format1{
     constructor(x=0){
-        this.piece =  Array(new Block('#372780', 90-x/2, x), new Block('#372780', 120-x/2, x), new Block('#372780', 120-x/2, x+30), new Block('#372780', 150-x/2, x));
+        // this.piece =  Array(new Block('#372780', 90-x/2, x), new Block('#372780', 120-x/2, x), new Block('#372780', 120-x/2, x+30), new Block('#372780', 150-x/2, x));
+        this.piece =  Array(new Block('#6A31D1', 90-x/2, x), new Block('#6A31D1', 120-x/2, x), new Block('#6A31D1', 120-x/2, x+30), new Block('#6A31D1', 150-x/2, x));
+                                // "#6A31BB" "#6A31D1" "#7344D1"
     }
 
     rotate(rotation){
@@ -48,9 +52,12 @@ export class Format1{
     }
 }
 
+// Verde -> --__ 
 export class Format2{
     constructor(x=0){
-        this.piece =  Array(new Block('#4f8d23', 90-x/2, x), new Block('#4f8d23', 120-x/2, x), new Block('#4f8d23', 120-x/2, x+30), new Block('#4f8d23', 150-x/2, x+30));
+        // this.piece =  Array(new Block('#4f8d23', 90-x/2, x), new Block('#4f8d23', 120-x/2, x), new Block('#4f8d23', 120-x/2, x+30), new Block('#4f8d23', 150-x/2, x+30));
+        // this.piece =  Array(new Block('#93C938', 90-x/2, x), new Block('#93C938', 120-x/2, x), new Block('#93C938', 120-x/2, x+30), new Block('#93C938', 150-x/2, x+30));
+        this.piece =  Array(new Block('#73BC38', 90-x/2, x), new Block('#73BC38', 120-x/2, x), new Block('#73BC38', 120-x/2, x+30), new Block('#73BC38', 150-x/2, x+30));
     }
     
     rotate(rotation){
@@ -83,9 +90,12 @@ export class Format2{
 
 }   
 
+// Laranja -> ----
 export class Format3{
     constructor(x=0){
-        this.piece =  Array(new Block('#e05d1a', 90-x/2, x), new Block('#e05d1a', 120-x/2, x), new Block('#e05d1a', 150-x/2, x), new Block('#e05d1a', 180-x/2, x));
+        // this.piece =  Array(new Block('#e05d1a', 90-x/2, x), new Block('#e05d1a', 120-x/2, x), new Block('#e05d1a', 150-x/2, x), new Block('#e05d1a', 180-x/2, x));
+        this.piece =  Array(new Block('#FFA704', 90-x/2, x), new Block('#FFA704', 120-x/2, x), new Block('#FFA704', 150-x/2, x), new Block('#FFA704', 180-x/2, x));
+                                // "#FFE02C"
     }
     
     rotate(rotation){ 
@@ -117,9 +127,13 @@ export class Format3{
     }
 }
 
+// _    <- Azul
+// ---  
 export class Format4{
     constructor(x=0){
-        this.piece =  Array(new Block('#2c5499', 90-x/2, x), new Block('#2c5499', 90-x/2, x+30), new Block('#2c5499', 120-x/2, x), new Block('#2c5499', 150-x/2, x));
+        // this.piece =  Array(new Block('#2c5499', 90-x/2, x), new Block('#2c5499', 90-x/2, x+30), new Block('#2c5499', 120-x/2, x), new Block('#2c5499', 150-x/2, x));
+        this.piece =  Array(new Block('#07ACD5', 90-x/2, x), new Block('#07ACD5', 90-x/2, x+30), new Block('#07ACD5', 120-x/2, x), new Block('#07ACD5', 150-x/2, x));
+                                // "#07ACE3" "#19ACC4" "#07ACD5"
     }
 
     rotate(rotation){
@@ -165,9 +179,12 @@ export class Format4{
     }
 }
 
+//   _   <- Amarelo
+// ---
 export class Format5{
     constructor(x=0){
-        this.piece =  Array(new Block('#e8c917', 90-x/2, x), new Block('#e8c917', 120-x/2, x), new Block('#e8c917', 150-x/2, x), new Block('#e8c917', 150-x/2, x+30));
+        // this.piece =  Array(new Block('#e8c917', 90-x/2, x), new Block('#e8c917', 120-x/2, x), new Block('#e8c917', 150-x/2, x), new Block('#e8c917', 150-x/2, x+30));
+        this.piece =  Array(new Block('#FFE02C', 90-x/2, x), new Block('#FFE02C', 120-x/2, x), new Block('#FFE02C', 150-x/2, x), new Block('#FFE02C', 150-x/2, x+30));
     }
     
     rotate(rotation){
@@ -213,9 +230,13 @@ export class Format5{
     }
 }
 
+// Vermelha -> __--
 export class Format6{
     constructor(x=0){
-        this.piece =  Array(new Block('#b9231f', 90-x/2, x+30), new Block('#b9231f', 120-x/2, x+30), new Block('#b9231f', 120-x/2, x), new Block('#b9231f', 150-x/2, x));
+        // this.piece =  Array(new Block('#b9231f', 90-x/2, x+30), new Block('#b9231f', 120-x/2, x+30), new Block('#b9231f', 120-x/2, x), new Block('#b9231f', 150-x/2, x));
+        // this.piece =  Array(new Block('#ff0000', 90-x/2, x+30), new Block('#ff0000', 120-x/2, x+30), new Block('#ff0000', 120-x/2, x), new Block('#ff0000', 150-x/2, x));
+        this.piece =  Array(new Block('#E92F3A', 90-x/2, x+30), new Block('#E92F3A', 120-x/2, x+30), new Block('#E92F3A', 120-x/2, x), new Block('#E92F3A', 150-x/2, x));
+                                    // "#E92F3A" "#DF2F3A" "#E8413A"
     }
     
     rotate(rotation){
@@ -247,9 +268,12 @@ export class Format6{
     }
 }
 
+// [] <- rosa
 export class Format7{
     constructor(x=0){
-        this.piece =  Array(new Block('#ed4974', 120-x/2, x), new Block('#ed4974', 120-x/2, x+30), new Block('#ed4974', 150-x/2, x), new Block('#ed4974', 150-x/2, x+30));
+        // this.piece =  Array(new Block('#ed4974', 120-x/2, x), new Block('#ed4974', 120-x/2, x+30), new Block('#ed4974', 150-x/2, x), new Block('#ed4974', 150-x/2, x+30));
+        this.piece =  Array(new Block('#E75FAD', 120-x/2, x), new Block('#E75FAD', 120-x/2, x+30), new Block('#E75FAD', 150-x/2, x), new Block('#E75FAD', 150-x/2, x+30));
+                                // "#E75F94" "#E75FAD" "#FF2444"
     }
 
     rotate(){
