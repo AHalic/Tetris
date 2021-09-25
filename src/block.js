e40756160b6c68b23f83727435cbeb9b0a98f7cf
AHalic <sophie.gama@edu.ufes.br>
2021-09-25 16:37:16 -0300

    Adiciona comentarios a classe Block

--- src/block.js
+++ src/block.js
@@ -1,67 +1,58 @@
+
+/**
+ * Classe representação de um bloco de uma peça
+ */
 class Block {
+    /**
+     * Constrói o bloco
+     * @param {string} color hexadecimal referente a cor da peça
+     * @param {number} x posição no jogo
+     * @param {number} y posição no jogo
+     * @param {BlockImages} BlockImages classe contendo as imagens do bloco
+     */
     constructor(color, x, y, BlockImages) {
         this.color = color;
-        // posições no canva
+
+        /* Para achar a posição na matriz basta dividir por 30 */
         this.x = x;
         this.y = y;
+
         this.blockImg = BlockImages.whichImage(color);
-        /* Para achar a posição na matriz basta dividir por 30 */
     }
 
-    static whichColor(hexColor){
-        let str_color = undefined;
-        switch (hexColor) {
-            case '#6A31D1': str_color = 'purple';
-                break;
-            case '#73BC38': str_color = 'green';
-                break;
-            case '#FFA704': str_color = 'orange';
-                break;
-            case '#07ACD5': str_color = 'blue';
-                break;
-            case '#FFE02C': str_color = 'yellow';
-                break;
-            case '#E92F3A': str_color = 'red';
-                break;
-            case '#E75FAD': str_color = 'magenta';
-                break;
-        }
-        if (str_color === undefined){
-            console.log("Something went wrong, color is undefined");
-        }
-        return str_color;
-    }
-
-    static defineColorBlock(hexColor){
-        let source = "imgs/blocks/";
-        source += Block.whichColor(hexColor) + "Block.png";
-        return source;
-    }
-
+    /**
+     * Desenha o bloco
+     * @param {*} boardContext board em que o bloco será desenhado
+     */
     drawBlock(boardContext) {
-        // let blockImg = new Image();
-        // blockImg.src = Block.defineColorBlock(this.color);
         boardContext.drawImage(this.blockImg, this.x, this.y, 30, 30);
 
         boardContext.lineWidth = 3;
         boardContext.strokeStyle = '#17181B';
         boardContext.lineCap = 'round';
         boardContext.strokeRect(this.x , this.y , 30, 30);
-
     }
 
+    /**
+     * Desce o bloco no jogo
+     */    
     down(){
         this.y += 30;
     }
 
+    /**
+     * Movimenta o bloco para a esquerda
+     */
     left() {
         this.x -= 30;
     }
     
+    /**
+     * Movimenta o bloco para a direita
+     */
     right() {
         this.x += 30;
     }
-
 }
 
 export default Block;
